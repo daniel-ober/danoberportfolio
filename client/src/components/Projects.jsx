8a0b27da8b0e1f1f3d8a28ac7c7824bf9e31d1c3
Daniel Ober <ChillDrummer@MacBook-Pro.local>
2021-04-11 00:37:20 -0500

    projects images added

--- client/src/components/Projects.jsx
+++ client/src/components/Projects.jsx
@@ -5,8 +5,11 @@ export default function Projects() {
     <div className="projects-container">
       <h2>PROJECTS</h2>
       <h3>Sunday Driver</h3>
+      <a href='https://sundaydriver.netlify.app/'><img className='project-image' src='https://i.imgur.com/ICX5fh8.png' alt='sunday-driver'></img></a>
       <h3>doberpop</h3>
+      <a href='https://doberpop.netlify.app/'><img className='project-image' src='https://i.imgur.com/FVo119M.png' alt='doberpop'></img></a>
       <h3>ARTIST ABC</h3>
+      <a href='https://artist-abc.netlify.app/'><img className='project-image' src='https://i.imgur.com/1gHVjzS.png' alt='artist-abc'></img></a>
       <h3>Hot Chicken Heroes</h3>
     </div>
   );
