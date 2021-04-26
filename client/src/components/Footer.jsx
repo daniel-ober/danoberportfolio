abc116c900cb155a83267d32e88d1aa06c9f9fe6
Daniel Ober <ChillDrummer@MacBook-Pro.local>
2021-04-26 00:42:38 -0500

    css enhancements

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -4,11 +4,11 @@ import React from "react";
 function Footer() {
   return (
     <div className='footer'>
-      {/* <NavLink className="footer-logo" to="/">
-        DAN<strong>OBER</strong>
-      </NavLink> */}
-        <a href="https://github.com/daniel-ober" rel="noopener noreferrer" target="_blank">GitHub</a>
-        <a href="https://www.linkedin.com/in/daniel-ober/" rel="noopener noreferrer" target="_blank">Linkedin</a>
+      <div className='footer-copyright'>Copyright &copy; 2021, Dan Ober</div>
+      <div className='footer-links'>
+        <a href="https://github.com/daniel-ober" rel="noopener noreferrer" target="_blank"><img src='https://i.imgur.com/j9xJnr7.png' alt='linkedin-icon'></img></a>
+        <a href="https://www.linkedin.com/in/daniel-ober/" rel="noopener noreferrer" target="_blank"><img src='https://i.imgur.com/mJqNCev.png' alt='github-icon'></img></a>
+        </div>
     </div>
   );
 }
